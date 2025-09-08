43ba0c12d314c50d7f5d540eb69b6bd88e80e5a6
Anush-Shand <127097095+Anush-Shand@users.noreply.github.com>
2025-09-08 15:37:11 +0530

    task(SDK-5047) - Adds vanniktech plugin for publishing (#5)
    * task(SDK-5047) - Adds vanniktech plugin for publishing
    
    * task(SDK-5047) - Coderabbit comments
    
    * task(SDK-5047) - Removes task to set build name

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,10 @@
-val GROUP: String by project
-val VERSION_NAME: String by project
-
-group = GROUP
-version = getVersionName()
-
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
     id("org.jetbrains.kotlin.jvm") version "2.0.10"
 
     // Apply the java-library plugin for API and implementation separation.
     `java-library`
-    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
-
+    id("com.vanniktech.maven.publish") version "0.34.0" apply false
 }
 
 buildscript {
@@ -36,15 +29,3 @@ allprojects {
         gradlePluginPortal()
     }
 }
-
-nexusPublishing {
-    repositories {
-        sonatype()
-    }
-}
-
-fun getVersionName() =
-    if (hasProperty("release"))
-        VERSION_NAME
-    else
-        "$VERSION_NAME-SNAPSHOT"
